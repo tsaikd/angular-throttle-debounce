717f2c4d20abdc9466841d588c31fd78c3613c46
tsaikd <tsaikd@gmail.com>
2015-06-14 09:52:21 +0800

    0.0.1 (2c3c5fa) compile
    Signed-off-by: tsaikd <tsaikd@gmail.com>

---
+++ dist/angular-throttle-debounce.js
@@ -0,0 +1,56 @@
+angular
+
+.module("angular-throttle-debounce", [])
+
+// https://css-tricks.com/the-difference-between-throttling-and-debouncing/
+// https://remysharp.com/2010/07/21/throttling-function-calls
+.factory("throttle"
+	, [       "$q", "$timeout"
+	, function($q,   $timeout) {
+	return function(callback, threshhold) {
+		threshhold = threshhold || 250;
+
+		var last, deferTimer;
+
+		return function() {
+			var now = +new Date(),
+				args = arguments,
+				context = this;
+
+			if (last && now < (last + threshhold)) {
+				$timeout.cancel(deferTimer);
+				deferTimer = $timeout(function() {
+					last = now;
+					callback.apply(context, args);
+				}, threshhold);
+				return deferTimer;
+			} else {
+				last = now;
+				callback.apply(context, args);
+				return $timeout(function() {}, 0);
+			}
+		};
+	};
+}])
+
+.factory("debounce"
+	, [       "$timeout"
+	, function($timeout) {
+	return function(callback, delay) {
+		delay = delay || 250;
+
+		var deferTimer;
+
+		return function() {
+			var args = arguments,
+				context = this;
+			$timeout.cancel(deferTimer);
+			deferTimer = $timeout(function() {
+				callback.apply(context, args);
+			}, delay);
+			return deferTimer;
+		};
+	};
+}])
+
+;
